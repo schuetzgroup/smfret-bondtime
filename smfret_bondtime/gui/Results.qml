285fa9c3b1528653cca614ec161c6d4a99e7f036
Lukas Schrangl <lukas.schrangl@boku.ac.at>
2024-06-20 11:46:12 +0200

    Results GUI: Support exporting data and figure

--- smfret_bondtime/gui/Results.qml
+++ smfret_bondtime/gui/Results.qml
@@ -1,5 +1,6 @@
 import QtQuick 2.15
 import QtQuick.Controls 2.15
+import QtQuick.Dialogs 1.3 as QQDialogs
 import QtQuick.Layouts 1.15
 import SdtGui 0.2 as Sdt
 
@@ -66,7 +67,34 @@ T.Results {
                 text: "Calculate lifetime…"
                 Layout.columnSpan: 3
                 Layout.fillWidth: true
-                onPressed: root.calculate()
+                onPressed: {
+                    workerDialog.title = "Calculating lifetime…"
+                    root.calculate()
+                }
+            }
+            Item {
+                height: 5
+                Layout.columnSpan: 3
+            }
+            Button {
+                text: "Export results…"
+                Layout.columnSpan: 3
+                enabled: root.resultAvailable
+                Layout.fillWidth: true
+                onPressed: {
+                    workerDialog.title = "Exporting results…"
+                    exportFileDialog.open()
+                }
+            }
+            Button {
+                text: "Save plot…"
+                Layout.columnSpan: 3
+                enabled: root.resultAvailable
+                Layout.fillWidth: true
+                onPressed: {
+                    workerDialog.title = "Exporting plot…"
+                    exportFigDialog.open()
+                }
             }
             Item {
                 Layout.columnSpan: 3
@@ -81,29 +109,70 @@ T.Results {
     }
 
     Dialog {
-        id: calcDialog
-        title: root.calcError === "" ? "Calculating lifetime…" : "Error"
+        id: workerDialog
         anchors.centerIn: Overlay.overlay
         closePolicy: Popup.NoAutoClose
         modal: true
-        standardButtons: root.calcError === "" ? Dialog.Cancel : Dialog.Ok
-        visible: root._calcWorker.busy || root.calcError !== ""
+        visible: root._worker.busy
         contentHeight: Math.max(progBar.implicitHeight, errText.implicitHeight)
 
         ProgressBar {
             id: progBar
             anchors.fill: parent
             indeterminate: true
-            visible: root._calcWorker.busy
+            visible: root._worker.busy
+
+            states: [
+                State {
+                    name: "error"
+                    when: root._workerError !== ""
+                    PropertyChanges {
+                        target: workerDialog
+                        title: "Error"
+                        visible: true
+                    }
+                    PropertyChanges {
+                        target: workerDialogButton
+                        text: "Close"
+                        DialogButtonBox.buttonRole: DialogButtonBox.AcceptRole
+                    }
+                }
+            ]
         }
         Text {
             id: errText
             anchors.fill: parent
-            visible: !root._calcWorker.busy
-            text: root.calcError
+            text: root._workerError
             wrapMode: Text.Wrap
         }
 
-        onRejected: { root._calcWorker.enabled = false }
+        footer: DialogButtonBox {
+            // If we used `standardButtons`, we'd get a binding loop on implicitHeight
+            Button {
+                id:workerDialogButton
+                text: "Abort"
+                DialogButtonBox.buttonRole: DialogButtonBox.RejectRole
+            }
+        }
+
+        onRejected: { root._worker.enabled = false }
+    }
+
+    QQDialogs.FileDialog {
+        id: exportFileDialog
+        selectMultiple: false
+        selectExisting: false
+        nameFilters: ["Excel (*.xlsx)", "Open Document (*.ods)"]
+        folder: Sdt.Sdt.parentUrl(backend.saveFile)
+        onAccepted: { root.exportResults(fileUrl) }
+    }
+
+    QQDialogs.FileDialog {
+        id: exportFigDialog
+        selectMultiple: false
+        selectExisting: false
+        nameFilters: ["SVG (*.svg)", "PDF (*.pdf)", "PNG (*.png)"]
+        folder: Sdt.Sdt.parentUrl(backend.saveFile)
+        onAccepted: { root.exportFigure(fileUrl) }
     }
 }
--- smfret_bondtime/gui/results.py
+++ smfret_bondtime/gui/results.py
@@ -1,9 +1,10 @@
 import random
 
 from PyQt5 import QtCore, QtQml, QtQuick
+import pandas as pd
 from sdt import gui
 
-from ..analysis import LifetimeAnalyzer
+from ..analysis import LifetimeAnalyzer, concat_stats
 
 
 class Results(QtQuick.QQuickItem):
@@ -13,7 +14,6 @@ class Results(QtQuick.QQuickItem):
     minCount = gui.QmlDefinedProperty()
     nBoot = gui.QmlDefinedProperty()
     randomSeed = gui.QmlDefinedProperty()
-    calcError = gui.SimpleQtProperty(str)
 
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -21,25 +21,43 @@ class Results(QtQuick.QQuickItem):
         self._datasets = None
         self._minLength = 1
         self._calcError = ""
+        self._analyzer = None
 
-        self._worker = gui.ThreadWorker(self._calcFunc)
-        self._worker.finished.connect(self._workerFinished)
-        self._worker.error.connect(self._workerError)
+        self._wrk = gui.ThreadWorker(self._workerDispatch)
+        self._wrk.finished.connect(self._wrkFinishedOk)
+        self._wrk.error.connect(self._wrkFinishedError)
+
+        self._wrkError = ""
 
     @QtCore.pyqtProperty(QtCore.QObject, constant=True)
-    def _calcWorker(self):
-        return self._worker
+    def _worker(self):
+        return self._wrk
+
+    resultAvailableChanged = QtCore.pyqtSignal()
+
+    @QtCore.pyqtProperty(bool, notify=resultAvailableChanged)
+    def resultAvailable(self):
+        return self._analyzer is not None
+
+    _workerErrorChanged = QtCore.pyqtSignal()
+
+    @QtCore.pyqtProperty(str, notify=_workerErrorChanged)
+    def _workerError(self):
+        return self._wrkError
 
     @QtCore.pyqtSlot(result=int)
     def genRandomSeed(self):
         # at least on Windows only 32 bit can be used
-        return random.randint(0, 1<<32-1)
+        return random.randint(0, 1 << 32 - 1)
 
     @QtCore.pyqtSlot()
     def calculate(self):
-        self.calcError = ""
-        self._worker.enabled = True
-        self._worker(
+        if self._wrkError:
+            self._wrkError = ""
+            self._workerErrorChanged.emit()
+        self._wrk.enabled = True
+        self._wrk(
+            "calculate",
             self.datasets,
             self.resultsFig.figure,
             self.minLength,
@@ -47,6 +65,35 @@ class Results(QtQuick.QQuickItem):
             self.nBoot,
             self.randomSeed,
         )
+
+    @QtCore.pyqtSlot(QtCore.QUrl)
+    def exportResults(self, url):
+        if self._wrkError:
+            self._wrkError = ""
+            self._workerErrorChanged.emit()
+        self._wrk.enabled = True
+        self._wrk("export_results", url, self._analyzer)
+
+    @QtCore.pyqtSlot(QtCore.QUrl)
+    def exportFigure(self, url):
+        if self._wrkError:
+            self._wrkError = ""
+            self._workerErrorChanged.emit()
+        self._wrk.enabled = True
+        self._wrk("export_figure", url, self.resultsFig.figure)
+
+    @staticmethod
+    def _workerDispatch(action, *args, **kwargs):
+        if action == "calculate":
+            ret = __class__._calcFunc(*args, **kwargs)
+        elif action == "export_results":
+            ret = __class__._exportResultsFunc(*args, **kwargs)
+        elif action == "export_figure":
+            ret = __class__._exporFigureFunc(*args, **kwargs)
+        else:
+            raise ValueError(f"unknown action {action}")
+
+        return action, ret
 
     @staticmethod
     def _calcFunc(datasets, fig, minLength, minCount, nBoot, randomSeed):
@@ -75,6 +122,7 @@ class Results(QtQuick.QQuickItem):
         if not fig.axes:
             fig.add_subplot(1, 2, 1)
             fig.add_subplot(1, 2, 2)
+            fig.set_constrained_layout(True)
         for a in fig.axes:
             a.cla()
         ana.plot(fig.axes[0])
@@ -83,12 +131,40 @@ class Results(QtQuick.QQuickItem):
             a.legend(loc=0)
         fig.canvas.draw_idle()
 
-    def _workerFinished(self):
-        self._worker.enabled = False
+        return ana
 
-    def _workerError(self, e):
-        self.calcError = str(e)
-        self._worker.enabled = False
+    @staticmethod
+    def _exportResultsFunc(url, analyzer):
+        with pd.ExcelWriter(url.toLocalFile()) as ew:
+            lt = analyzer.lifetime
+            res = pd.DataFrame(
+                [[lt.lifetime, lt.lifetime_err], [lt.bleach, lt.bleach_err]],
+                index=["lifetime", "bleach"],
+                columns=["value", "standard error"],
+            )
+            res.to_excel(ew, sheet_name="lifetime")
+            analyzer.apparent_lifetimes.to_excel(ew, sheet_name="apparent lifetimes")
+
+            tstats = concat_stats(analyzer.track_stats, filter=False)
+            for intv, ts in tstats.items():
+                ts.to_excel(ew, sheet_name=f"interval {intv}")
+
+    @staticmethod
+    def _exporFigureFunc(url, figure):
+        figure.savefig(url.toLocalFile())
+
+    def _wrkFinishedOk(self, result):
+        self._wrk.enabled = False
+        if result[0] == "calculate":
+            a = self.resultAvailable
+            self._analyzer = result[1]
+            if not a:
+                self.resultAvailableChanged.emit()
+
+    def _wrkFinishedError(self, e):
+        self._wrkError = str(e)
+        self._workerErrorChanged.emit()
+        self._wrk.enabled = False
 
 
 QtQml.qmlRegisterType(Results, "SmFretBondTime.Templates", 1, 0, "Results")
