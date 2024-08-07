089a38fe84d9adb8906a7054be5ca2a156a10588
Lukas Schrangl <lukas.schrangl@boku.ac.at>
2024-08-07 11:51:41 +0200

    Define entry point for GUI in pyproject.toml

--- smfret_bondtime/__main__.py
+++ smfret_bondtime/__main__.py
@@ -2,39 +2,9 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
-import argparse
-from pathlib import Path
 import sys
 
-from PyQt5 import QtWidgets
-import matplotlib as mpl
-from sdt import gui
-
-# need to import so that QML types get registered
-from . import gui as bond_gui  # noqa: F401
-from ._version import __version__
+from . import gui
 
 
-mpl.rcParams["axes.unicode_minus"] = False
-
-app = QtWidgets.QApplication(sys.argv)
-app.setOrganizationName("schuetzgroup")
-app.setOrganizationDomain("biophysics.iap.tuwien.ac.at")
-app.setApplicationName("SmFretBondTime")
-app.setApplicationVersion(__version__)
-
-argp = argparse.ArgumentParser(description="Analyze bond lifetimes via smFRET data")
-argp.add_argument("save", help="Save file", nargs="?", type=Path)
-args = argp.parse_args()
-
-if sys.platform != "win32":
-    gui.mpl_use_qt_font()
-
-comp = gui.Component(Path(__file__).parent / "gui" / "main.qml")
-comp.create()
-if comp.status_ == gui.Component.Status.Error:
-    sys.exit(1)
-if args.save is not None:
-    comp.backend.load(args.save.resolve())
-
-sys.exit(app.exec_())
+sys.exit(gui.run())
--- smfret_bondtime/gui/__init__.py
+++ smfret_bondtime/gui/__init__.py
@@ -2,9 +2,44 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
+import argparse
+from pathlib import Path
+import sys
+
+from PyQt5 import QtWidgets
+import matplotlib as mpl
+from sdt import gui
+
 from .backend import Backend
 from .changepoints import Changepoints
 from .filter import Filter
 from .image_pipeline import LifetimeImagePipeline
 from .results import Results
 from .track_navigator import TrackNavigator
+from .._version import __version__
+
+
+def run():
+    mpl.rcParams["axes.unicode_minus"] = False
+
+    app = QtWidgets.QApplication(sys.argv)
+    app.setOrganizationName("schuetzgroup")
+    app.setOrganizationDomain("biophysics.iap.tuwien.ac.at")
+    app.setApplicationName("SmFretBondTime")
+    app.setApplicationVersion(__version__)
+
+    argp = argparse.ArgumentParser(description="Analyze bond lifetimes via smFRET data")
+    argp.add_argument("save", help="Save file", nargs="?", type=Path)
+    args = argp.parse_args()
+
+    if sys.platform != "win32":
+        gui.mpl_use_qt_font()
+
+    comp = gui.Component(Path(__file__).parent / "main.qml")
+    comp.create()
+    if comp.status_ == gui.Component.Status.Error:
+        return 1
+    if args.save is not None:
+        comp.backend.load(args.save.resolve())
+
+    return app.exec_()
