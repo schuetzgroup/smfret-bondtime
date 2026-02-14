3a3f5ee8b0f380bee218470db38428f58f21b805
Lukas Schrangl <lukas.schrangl@boku.ac.at>
2026-02-15 00:39:54 +0100

    Set __version__ in _version.py so it can be imported also in gui

--- smfret_bondtime/__init__.py
+++ smfret_bondtime/__init__.py
@@ -1,9 +1,7 @@
 # SPDX-FileCopyrightText: 2024 Lukas Schrangl <lukas.schrangl@boku.ac.at>
 #
 # SPDX-License-Identifier: BSD-3-Clause
-import importlib.metadata
 
+from ._version import __version__
 from .analysis import LifetimeAnalyzer, LifetimeResult, calc_track_stats
 from .io import load_data, save_data
-
-__version__ = importlib.metadata.version("smfret-bondtime")
---
+++ smfret_bondtime/_version.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2024 Lukas Schrangl <lukas.schrangl@boku.ac.at>
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+import importlib.metadata
+
+__version__ = importlib.metadata.version("smfret-bondtime")
